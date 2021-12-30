87bdbcc3dbde831f739f7d605d9bd43553d74789
jaejun Kim <nicy130402@naver.com>
2021-12-31 00:01:21 +0900

    making query for table

--- src/routes/data.js
+++ src/routes/data.js
@@ -16,7 +16,30 @@ router.get('/test-list', (req, res) => {
     db.close()
 })
 
+router.get('/study', (req, res) => {
+    const tablename = req.query.table;
+    let db = new sqlite(path.join(__dirname, '../', 'database', 'database.db'));
+    const rows = db.prepare(`SELECT * FROM ${tablename}`).all();
+    
+    const filtered = rows.map(row => {
+        return row
+    })
 
+    res.send(filtered);
+    db.close()
+})
+
+router.get('/gogup', (req, res) => {
+    const tablename = req.query.table;
+    let db = new sqlite(path.join(__dirname, '../', 'database', 'database.db'));
+    const rows = db.prepare(`SELECT * FROM ${tablename}`).all();
+
+    const filtered = rows.filter((item, idx) => {
+        return item.to_user === 2
+    });
+    res.send(filtered);
+    db.close()
+})
 
 
 
